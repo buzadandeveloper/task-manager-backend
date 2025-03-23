a6d2cf1c642d832c33a7c2b46f1a957b4d45fabf
Buza Dan <buzadandeveloper@gmail.com>
2025-03-23 20:41:30 +0200

    fix: fixed google startegy

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -11,7 +11,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientID: `${process.env.GOOGLE_CLIENT_ID}`,
       clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
       callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
-      scope: ['email'],
+      scope: ['email', 'profile'],
     });
   }
 
