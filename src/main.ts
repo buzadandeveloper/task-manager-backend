ee163d25df382061634658dc6e54d378d5543fe7
Buza Dan <buzadandeveloper@gmail.com>
2025-05-25 18:44:24 +0300

    fix: fixed origin for cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: false,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -50,4 +50,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 5000);
 }
+
 void bootstrap();
